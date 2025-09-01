9db6d422271769a856443431b3abdee0ed556251
Arashpreet123 <pandherarashpreet@gmail.com>
2025-09-01 13:35:09 -0700

    add bootstrap icons for delete and edit job submission

--- frontend/src/components/JobList.js
+++ frontend/src/components/JobList.js
@@ -9,6 +9,8 @@ const JobList = ({ jobs }) => {
             <h5 className="card-title">{job.position} at {job.company}</h5>
             <p className="card-text">Status: {job.status}</p>
             <p className="card-text"><small>Date Applied: {job.date_applied}</small></p>
+            <a href="#"><i className="bi bi-pencil-square"></i></a>
+            <a href="#"><i className="bi bi-trash"></i></a>
           </div>
         </div>
       ))}
